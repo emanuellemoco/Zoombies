dec578ae75030ea7d0865a010d06eb8bace0b347
emanuellemoco <emanuellesmoco@gmail.com>
2021-05-21 09:23:45 -0300

    armas

---
+++ Assets/_Scripts/GunController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject holdpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.transform.SetParent(Camera.main.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
