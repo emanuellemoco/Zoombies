5dc650a04fda119850f8a9151410518f111df0f9
emanuellemoco <emanuellesmoco@gmail.com>
2021-06-10 19:53:47 -0300

    subindo

--- Assets/_Scripts/UI/UI_EndGame.cs
+++ Assets/_Scripts/UI/UI_EndGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class UI_EndGame : MonoBehaviour
@@ -9,8 +10,11 @@ public class UI_EndGame : MonoBehaviour
     // Start is called before the first frame update
     GameManager gm;
 
+    public Text message;
+
     public void Start(){
         gm = GameManager.GetInstance();
+        message.text =  $"You survived  {gm.round.ToString()} rounds";
     }
 
     public void Exit()
